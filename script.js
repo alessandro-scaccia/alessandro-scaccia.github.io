e922d8e0344f1c792b3525f4436ad0f2fc11f89b
Alessandro Scaccia <alessandro.scaccia99@gmail.com>
2024-01-05 23:59:00 +0100

    Prettier formatter

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 var countDownDate = new Date("Jan 10, 2024 17:00:00").getTime();
 
 var x = setInterval(function () {
+  var now = new Date().getTime();
 
-    var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-    var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "READY!!";
-    }
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "READY!!";
+  }
 }, 1000);
